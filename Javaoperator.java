873e9c7e90c4cdd11cd671c678c9b3696bd0fb10
Ankur Singh <132219013+ankursingh2005@users.noreply.github.com>
2024-05-09 12:23:44 +0530

    sem3 changes

--- Javaoperator.java
+++ Javaoperator.java
@@ -2,7 +2,7 @@ public class Javaoperator{
     public static void main(String[] agrs){
         int sum1=50;
         int sum2=sum1+100;
-        int sum3=sum2+200;
+        int sum3=sum2+300;
         System.out.println(sum1);
         System.out.println(sum2);
         System.out.println(sum3);
